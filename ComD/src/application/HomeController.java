6220d3f493cfa36770ec08b235b9c198d730facb
beltraoluis <beltraoluis@Aldebaran>
2017-09-17 13:35:40 -0300

    implementação dos atributos da classe HomeController com o inicio do construtor e alterações na interface gráfica Home.

--- ComD/src/application/HomeController.java
+++ ComD/src/application/HomeController.java
@@ -1,5 +1,33 @@
 package application;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class HomeController {
 
+	private Label origem;
+	private Label destino;
+	private TextField ipOrigem;
+	private TextField ipDestino;
+	private ScrollPane sp;
+	private VBox lista;
+	private TextArea mensagem;
+	private Button enviar;
+	
+	public HomeController() {
+		super();
+		this.origem = origem;
+		this.destino = destino;
+		this.ipOrigem = ipOrigem;
+		this.ipDestino = ipDestino;
+		this.sp = sp;
+		this.lista = lista;
+		this.mensagem = mensagem;
+		this.enviar = enviar;
+	}
+	
 }
