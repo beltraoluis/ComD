5d6bd38c5f57e7c0076014e051f9e49e910bfae7
Luís Henrique Beltrão Santana <luis.2012@alunos.utfpr.edu.br>
2017-11-04 15:29:30 -0200

    ajustes na interface

--- ComD/src/application/HomeController.java
+++ ComD/src/application/HomeController.java
@@ -1,9 +1,13 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -19,14 +23,29 @@ public class HomeController {
         @FXML
 	private TextField ipDestino;
 	private ScrollPane sp;
-	private VBox lista;
+        @FXML
+	private TextArea lista;
         @FXML
 	private TextArea mensagem;
 	private Button enviar;
         private Button conectar;
         	
 	public void conectarEvento(ActionEvent evento){
+            String ip = ipOrigem.getText();
             origem.setText("Origem conectada:");
+        }
+        
+        public void enviarEvento(ActionEvent evento){
+            Date agora = new Date();
+            SimpleDateFormat df = new SimpleDateFormat("H:mm ");
+            StringBuffer texto = new StringBuffer("");
+            texto.append(df.format(agora));
+            texto.append("- eu:\n");
+            texto.append(mensagem.getText());
+            texto.append("\n\n");
+            texto.append(lista.getText());
+            lista.setText(texto.toString());
+            mensagem.clear();
         }
 	
 }
